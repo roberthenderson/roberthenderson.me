b1c63caae5c51acc150da4a4a99d0f26c2b55d59
Robert Henderson <rhahenderson@gmail.com>
2025-01-23 15:11:39 -0700

    import default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,58 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
-module.exports = {
-  darkMode: 'selector',
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      spacing: {
-        18: '72px',
-      },
-      letterSpacing: {
-        wide: '0.01em',
-        wider: '0.02em',
-        tight: '-0.005em',
-      },
-      fontSize: {
-        logo: '2.5rem',
-      },
-      fontFamily: {
-        leagueSpartan: '"League Spartan", serif',
-        dmSerif: '"DM Serif Text", serif',
-        dmSans: '"DM Sans", serif',
-      },
-      fontWeight: {
-        500: 500,
-      },
-      borderWidth: {
-        3: '3px',
-      },
-      lineHeight: {
-        11: '2.75rem',
-        12: '3rem',
-        14: '3.5rem',
-        16: '4rem',
-      },
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-        icon: {
-          base: '#1d6368',
-          hover: '#b47409',
-        },
+import colors from 'tailwindcss/colors';
+
+export const darkMode = 'selector';
+export const content = ['./src/**/*.{js,ts,jsx,tsx}'];
+
+const entries = Object.entries(Object.getOwnPropertyDescriptors(colors)).filter(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  ([key, descriptor]) => {
+    return typeof descriptor.get !== 'function'; // Exclude getters
+  },
+);
+
+const nonDeprecatedColors = Object.fromEntries(
+  entries.map(([key, descriptor]) => [key, descriptor.value]),
+);
+
+export const theme = {
+  extend: {
+    spacing: {
+      18: '72px',
+    },
+    letterSpacing: {
+      wide: '0.01em',
+      wider: '0.02em',
+      tight: '-0.005em',
+    },
+    fontSize: {
+      logo: '2.5rem',
+    },
+    fontFamily: {
+      leagueSpartan: '"League Spartan", serif',
+      dmSerif: '"DM Serif Text", serif',
+      dmSans: '"DM Sans", serif',
+    },
+    fontWeight: {
+      500: 500,
+    },
+    borderWidth: {
+      3: '3px',
+    },
+    lineHeight: {
+      11: '2.75rem',
+      12: '3rem',
+      14: '3.5rem',
+      16: '4rem',
+    },
+    colors: {
+      ...nonDeprecatedColors,
+      background: 'var(--background)',
+      foreground: 'var(--foreground)',
+      border: {
+        card: nonDeprecatedColors.indigo[400],
       },
     },
   },
-  plugins: [],
 };
+export const plugins = [];
