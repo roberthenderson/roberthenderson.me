d9a6c1c63dbf1b08b8b11de038f0cd8fa8c7acfc
Robert Henderson <rhahenderson@gmail.com>
2025-01-31 13:48:11 -0700

    add og image to robots allow list

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
+      allow: ['/', '/api/og'],
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
   };
