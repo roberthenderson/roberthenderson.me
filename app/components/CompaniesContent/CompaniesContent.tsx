99c26ec095a98ececf7be6919a84cf309b64b607
Robert Henderson <rhahenderson@gmail.com>
2025-02-05 18:21:12 -0700

    restrict to md

--- app/components/CompaniesContent/CompaniesContent.tsx
+++ app/components/CompaniesContent/CompaniesContent.tsx
@@ -52,7 +52,7 @@ export const CompaniesContent: FC<CompaniesContentProps> = ({
               'scroller my-auto flex max-w-screen-xl flex-col gap-6 overflow-y-auto',
               'mx-auto px-6 pb-10 pt-12 md:px-10 md:pt-10',
               'text-left',
-              isModal && 'max-h-[800px]',
+              isModal && 'md:max-h-[800px]',
             )}
           >
             <SectionHeading className="flex-row justify-between text-left md:items-start">
