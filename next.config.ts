d8a43554a7cf26adc482e21c7f9c81ec71c01bed
Robert Henderson <rhahenderson@gmail.com>
2025-02-07 09:24:33 -0700

    change dev folder again

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  distDir: process.env.NODE_ENV === 'development' ? '.next/dev' : '.next',
+  distDir: process.env.NODE_ENV === 'development' ? '.next_dev' : '.next',
   reactStrictMode: false,
 };
 
